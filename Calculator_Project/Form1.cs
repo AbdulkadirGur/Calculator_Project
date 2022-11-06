7233653d77dbac5babf6e569c2a783f8261ceb9b
Abdulkadir Gür <58683068+AbdulkadirGur@users.noreply.github.com>
2022-11-06 18:53:42 +0300

    Design Calculator Screen

---
+++ Calculator_Project/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "0")
+            {
+                textBox2.Text = "1";
+            }
+            else
+            {
+                textBox2.Text += "1";
+            }
+        }
+    }
+}
